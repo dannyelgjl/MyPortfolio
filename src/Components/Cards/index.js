7fc8292970ce6e8c62f0775702f639c3c35cccc4
dannyelgjl <dannyelgjl@gmail.com>
2020-10-18 19:27:16 -0300

    finished about me

--- src/Components/Cards/index.js
+++ src/Components/Cards/index.js
@@ -1,20 +1,26 @@
 import React from 'react';
 
-import me from '../../assets/images/me.JPG';
+import me from '../../assets/images/m1.JPG';
 
 import './styles.css';
 
 const Cards = () => {
   return (
     <section className="about" id="about">
-    <div className="max-width">
-    <h2 className="title">About me</h2>
-      <div className="about-content">
-        <div className="collumn left">
-          <img src={me} alt=""/>
+      <div className="max-width">
+        <h2 className="title">About me</h2>
+          <div className="about-content">
+            <div className="column left">
+              <img src={me} alt=""/>
+            </div>
+
+            <div className="column rigth">
+              <div className="text">I'm Daniel and I'm <span>Software Developer</span> </div>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque ratione quam repellat odio reprehenderit obcaecati dignissimos eos optio architecto laborum? Qui voluptatem aspernatur illum maiores sint similique accusantium at ipsa molestias, reprehenderit veniam rerum, nemo perspiciatis neque, error ipsam. Modi voluptates cupiditate tenetur non consequatur, incidunt itaque? Amet expedita architecto, deleniti ullam quidem quas nihil quasi ducimus, mollitia iste impedit.</p>
+              <a href="">Dowload Cv</a>
+            </div>
         </div>
       </div>
-    </div>
   </section>
   ) 
 }
