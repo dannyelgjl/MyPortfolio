d2e8c6f52a3d3b3cf57387265b480e58e45544a0
dannyelgjl <dannyelgjl@gmail.com>
2020-10-18 14:25:15 -0300

    connecting with github api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
+import api from './services/api';
 
 function App() {
+
+
+  useEffect(() => {
+    api.get('users/dannyelgjl').then(response => {
+      console.log(response.data);    
+    });
+  },[])
+
   return (
     <div className="App">
      <h1>Me</h1>
