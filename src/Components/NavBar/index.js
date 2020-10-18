516f16247f0db963f25e03ce8674d98139767448
dannyelgjl <dannyelgjl@gmail.com>
2020-10-18 17:24:32 -0300

    Scroll navbar

--- src/Components/NavBar/index.js
+++ src/Components/NavBar/index.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './styles.css';
 
+
 const NavBar = () => {
+  const [navbar, setNavbar] = useState(false);
+
+  const changeBackground = () => {
+    console.log(window.scrollY);
+    if (window.scrollY >= 80) {
+      setNavbar(true);
+    }else{
+      setNavbar(false);
+    }
+  }
+  
+  window.addEventListener('scroll', changeBackground);
+
   return (
-    <div className="navbar">
+    <div className={navbar ? 'navbar active' : 'navbar'}>
       <div className="max-width" id="teste">
         <div className="logo"><a href="#">Portfo<span>lio</span></a></div>
             <ul className="menu">
@@ -15,6 +29,7 @@ const NavBar = () => {
               <li><a href="">Teams</a></li>
               <li><a href="">Contact</a></li>
             </ul>
+          <script src="../../script.js" ></script>
         
       </div>
     </div>
--- src/Page/Home/index.js
+++ src/Page/Home/index.js
@@ -15,7 +15,7 @@ const Home = () => {
         </div>
       </section>
 
-      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 196</p>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industryIpsum is simply dummy text of the printing and typesetting industry. Lorem IpIpsum is simply dummy text of the printing and typesetting industry. Lorem IpIpsum is simply dummy text of the printing and typesetting industry. Lorem IpIpsum is simply dummy text of the printing and typesetting industry. Lorem IpIpsum is simply dummy text of the printing and typesetting industry. Lorem IpIpsum is simply dummy text of the printing and typesetting industry. Lorem IpIpsum is simply dummy text of the printing and typesetting industry. Lorem IpIpsum is simply dummy text of the printing and typesetting industry. Lorem IpIpsum is simply dummy text of the printing and typesetting industry. Lorem IpIpsum is simply dummy text of the printing and typesetting industry. Lorem Ip. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 196</p>
     </>
   );
 }
